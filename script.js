c11895888bf9cb21cd13fc5bb42552ec71628724
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-29 17:01:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function increment(){
+  num.innerText = parseInt(num.innerText) + 1;
+}
+
+function decrement(){
+  num.innerText = parseInt(num.innerText) - 1;
+}
+
+function reset(){
+  num.innerText = 0;
+}
